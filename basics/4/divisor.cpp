dbb4cdac49efbff20272c0ef82f722f73c673c5f
Sameeksha Gupta <136142214+Sameekksshhaa@users.noreply.github.com>
2024-09-01 13:11:54 +0530

    sum of all divisor

--- basics/4/divisor.cpp
+++ basics/4/divisor.cpp
@@ -1,19 +1,33 @@
-//User function Template for C++
+// //User function Template for C++
+// class Solution
+// {
+// public:
+//     long long sumOfDivisors(int N)
+//     {
+//         int a=0;
+//         int b=1;
+//         for(int i=2; i<=N; i++){
+//             for(int j=i; j>0; j--){
+//                 if(i%j==0){
+//                     a=a+j;
+//                 }
+//             }
+//             b=b+a;
+//             a=0;
+//         }
+//         return b;
+//     }
 class Solution
 {
 public:
     long long sumOfDivisors(int N)
     {
-        int a=0;
-        int b=1;
-        for(int i=2; i<=N; i++){
-            for(int j=i; j>0; j--){
-                if(i%j==0){
-                    a=a+j;
-                }
-            }
-            b=b+a;
-            a=0;
+        long long sum = 0;
+
+        for (int i = 1; i <= N; i++) {
+            sum += (N / i) * i;
         }
-        return b;
+
+        return sum;
     }
+};
