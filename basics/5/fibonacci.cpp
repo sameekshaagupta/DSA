79204cc1c04757adfccbee1e849d74da7d316d33
Sameeksha Gupta <136142214+Sameekksshhaa@users.noreply.github.com>
2024-10-04 20:00:00 +0100

    initial

---
+++ basics/5/fibonacci.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int fib(int n) {
+        return fiboo(n);
+    }
+    int fiboo(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        int a=0;
+        int b=1;
+        int c=0;
+        for (int i = 2; i <= n; ++i) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b; 
+    }
+};
