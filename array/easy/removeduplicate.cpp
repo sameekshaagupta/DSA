8ad053f64536ce9e01111fabdb1cda31cc207025
Sameeksha Gupta <136142214+Sameekksshhaa@users.noreply.github.com>
2024-10-11 17:00:46 +0530

    remove duplicate elements from vector optimized way

--- array/easy/removeduplicate.cpp
+++ array/easy/removeduplicate.cpp
@@ -17,3 +17,17 @@ public:
         return k;
     }
 };
+
+//optimized way
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j=1;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]!=nums[i-1]){
+                nums[j++]=nums[i];
+            }
+        }
+        return j;
+    }
+};
