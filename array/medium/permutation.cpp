ae48deae1aca9ba102eb6e2ae40aec4a0914394b
Sameeksha Gupta <136142214+Sameekksshhaa@users.noreply.github.com>
2024-12-13 22:02:05 +0530

    next permutation

---
+++ array/medium/permutation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+
+        // Step 1: Find the first decreasing element
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            // Step 2: Find the smallest element larger than nums[i] to the right
+            int j = n - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+
+        // Step 3: Reverse the suffix
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
