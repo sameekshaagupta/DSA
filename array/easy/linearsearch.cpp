ad666c8873d8556b2bffcc33b9dd4b636d55a635
Sameeksha Gupta <sameeksha.gce22@sot.pdpu.ac.in>
2025-06-21 18:47:43 +0530

    linear search

--- array/easy/linearsearch.cpp
+++ array/easy/linearsearch.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
-  public:
-    int searchInSorted(int arr[], int N, int K) {
-        for(int i=0;i<N;i++){
-            if(arr[i]==K){
-                return 1;
+public:
+    int linearSearch(vector<int>& nums, int target) {
+        for(int i =0; i<nums.size(); i++){
+            if(nums[i]==target){
+                return i;
             }
         }
         return -1;
