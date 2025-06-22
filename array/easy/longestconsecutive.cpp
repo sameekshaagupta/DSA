aa4e94575fe4b42e41d8ae719ff6612093351b64
Sameeksha Gupta <sameeksha.gce22@sot.pdpu.ac.in>
2025-06-22 15:45:39 +0530

    longest subsequence of 1

--- array/easy/longestconsecutive.cpp
+++ array/easy/longestconsecutive.cpp
@@ -18,3 +18,24 @@ public:
         return currentcount > maxcount ? currentcount : maxcount;
     }
 };
+
+//again
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count=0;
+        int temp=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==1){
+                temp++;
+                if(temp>count){
+                    count=temp;
+                }
+            }
+            else{
+                temp=0;
+            }
+        }
+        return count;
+    }
+};
