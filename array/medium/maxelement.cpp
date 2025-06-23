202f491d8be2b7b543f73760642717f7727df169
Sameeksha Gupta <sameeksha.gce22@sot.pdpu.ac.in>
2025-06-23 20:07:10 +0530

    finding max element using moores law

--- array/medium/maxelement.cpp
+++ array/medium/maxelement.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int element=0;
-        int count =0;
+        int count=0;
         for(int i=0; i<nums.size(); i++){
-            if(nums[i]==0){
+            if(count == 0){
                 element=nums[i];
                 count++;
             }
             else if(nums[i]==element){
                 count++;
             }
-            else{
+            else if(nums[i]!=element){
                 count--;
             }
         }
-        int cut=0;
-        for(int i=0;i<nums.size(); i++){
+        int cnt=0;
+        for(int i=0; i<nums.size(); i++){
             if(nums[i]==element){
-                cut++;
+                cnt++;
             }
         }
-        if(cut >= nums.size()/2){
+        if(cnt>(nums.size()/2)){
             return element;
         }
         return -1;
