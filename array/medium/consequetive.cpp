562f4c62026dd7ac797049bc54d3649a9ff6b1fb
Sameeksha Gupta <sameeksha.gce22@sot.pdpu.ac.in>
2025-06-29 11:54:26 +0530

    longest consequetive in array

---
+++ array/medium/consequetive.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int longest=1;
+        int n=nums.size();
+        if(n==0) return 0;
+        unordered_set<int> st;
+        for(int i=0; i<n; i++){
+            st.insert(nums[i]);
+            }
+        for(auto it:st){
+            if(st.find(it-1)==st.end()){
+                int cn=1;
+                int x=it;
+                while(st.find(x+1)!=st.end()){
+                    x++;
+                    cn++;
+                }
+                longest = max(longest,cn);
+            }
+        }
+        return longest;
+    }
+};
