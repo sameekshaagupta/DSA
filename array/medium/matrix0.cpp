746d30e8a70fa2fb8065a1bc3798d91e14f68f63
Sameeksha Gupta <sameeksha.gce22@sot.pdpu.ac.in>
2025-06-29 13:35:36 +0530

    matrix problem

--- array/medium/matrix0.cpp
+++ array/medium/matrix0.cpp
@@ -23,3 +23,46 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int firstColHasZero = 1;
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        // First pass to mark zeros
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) firstColHasZero = 0;
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Second pass to set zeros (excluding first row/column)
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Zero first row if needed
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Zero first column if needed
+        if (firstColHasZero == 0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
